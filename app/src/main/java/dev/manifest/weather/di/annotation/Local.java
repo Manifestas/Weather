215ae6fbcb2ef80373c7c481b527ab0c27f6bf6a
tv.unimol <doslovno@gmail.com>
2019-03-25 21:49:29 +0300

    add dagger2 annotations

---
+++ app/src/main/java/dev/manifest/weather/di/annotation/Local.java
@@ -0,0 +1,13 @@
+package dev.manifest.weather.di.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Local {
+}
