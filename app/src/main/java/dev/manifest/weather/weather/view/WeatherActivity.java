20adff4525c06d3f4db4d57fa4decad3011b5cad
tv.unimol <doslovno@gmail.com>
2019-04-07 19:49:59 +0300

    initViews

--- app/src/main/java/dev/manifest/weather/weather/view/WeatherActivity.java
+++ app/src/main/java/dev/manifest/weather/weather/view/WeatherActivity.java
@@ -1,35 +1,57 @@
 package dev.manifest.weather.weather.view;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import dev.manifest.weather.App;
 import dev.manifest.weather.R;
+import dev.manifest.weather.data.model.City;
+import dev.manifest.weather.di.component.DaggerWeatherComponent;
+import dev.manifest.weather.di.module.WeatherPresenterModule;
+import dev.manifest.weather.weather.WeatherContract;
+import dev.manifest.weather.weather.WeatherPresenter;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+public class WeatherActivity extends AppCompatActivity implements WeatherContract.View {
 
-public class WeatherActivity extends AppCompatActivity {
+    @Inject
+    WeatherPresenter presenter;
+    @Inject
+    WeatherAdapter adapter;
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView recyclerView;
+    private FloatingActionButton addCityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        DaggerWeatherComponent.builder()
+                .repositoryComponent(((App) getApplication()).getRepositoryComponent())
+                .weatherPresenterModule(new WeatherPresenterModule(this))
+                .build()
+                .injectInto(this);
+
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        presenter.subscribe();
+        presenter.updateData(true);
     }
 
     @Override
@@ -52,5 +74,70 @@ public class WeatherActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void showProgress() {
+
+    }
+
+    @Override
+    public void showData(List<City> cities) {
+
+    }
+
+    @Override
+    public void showError() {
+
+    }
+
+    @Override
+    public void hideItemAtPosition(int position) {
+
+    }
+
+    @Override
+    public void showEmptyState() {
+
+    }
+
+    @Override
+    public void showNetworkErrorNotification() {
+
+    }
+
+    @Override
+    public void showCurrentCityDeletionErrorNotification() {
+
+    }
+
+    private void initViews() {
+        initRecyclerView();
+
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(this::fetchData);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        addCityButton = findViewById(R.id.fab);
+        addCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+    }
+
+    private void fetchData() {
+        presenter.updateData(true);
     }
 }
